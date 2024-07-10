cc79067a0a41a4261b2f6efde40721dd6d537384
Rick Sanchez <tyronemguni@gmail.com>
2024-07-10 17:54:34 +0200

    Auto deploy staging

--- app/start_app.sh
+++ app/start_app.sh
@@ -1,2 +1,2 @@
-kill $(lsof -t -i:8000)
-gunicorn config.wsgi:application --bind 0.0.0.0:8000  --workers 3 --log-level=info --log-file=- --access-logfile=- --timeout 120
+kill $(lsof -t -i:7000)
+gunicorn config.wsgi:application --bind 0.0.0.0:7000  --workers 3 --log-level=info --log-file=- --access-logfile=- --timeout 120
