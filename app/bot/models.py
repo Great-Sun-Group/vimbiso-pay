a0fa07eb86ea95037c5f6e8c6532537f909b019d
Takudzwa Sharara <shararagarnet@gmail.com>
2024-07-25 18:24:31 +0200

    Restructuring flow

--- app/bot/models.py
+++ app/bot/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Message(models.Model):
     """Model For Wallet Message."""
     messsage = models.TextField()
+    last_updated = models.DateTimeField(auto_now=True)
